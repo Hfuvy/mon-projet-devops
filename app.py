2c1d1551d9b8dec7e17c03097ebb91821b0004ab
udcw <udemycodewarrios@gmail.com>
2026-07-16 16:38:00 +0200

    Mise à jour vers la Version 2.0

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f" DevOps en action ! Environnement : {os.environ.get('ENV', 'local')}"
+    # On a changé le message pour marquer la version 2 !
+    return f"DevOps en action ! Version 2.0 - Env : {os.environ.get('ENV', 'local')}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
